8dabd301fad6d8d50896f98ba29fd991c5811fce
RawalAnuma <rawalanuma@gmail.com>
2026-08-11 19:18:21 +0545

    feat: register category provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:restaurant_management_app/providers/category_provider.dart';
 import 'package:restaurant_management_app/providers/food_provider.dart';
 import 'package:restaurant_management_app/screens/food_screen.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(create: (_) => FoodProvider(), child: const MyApp()),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => FoodProvider()),
+        ChangeNotifierProvider(create: (_) => CategoryProvider()),
+      ],
+      child: const MyApp(),
+    ),
   );
 }
 
