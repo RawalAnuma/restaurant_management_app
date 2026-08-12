732668519afc32497202bc54bfc7c17213ec65f5
RawalAnuma <rawalanuma@gmail.com>
2026-08-12 10:07:43 +0545

    feat: configure app providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:restaurant_management_app/providers/auth_provider.dart';
 import 'package:restaurant_management_app/providers/category_provider.dart';
 import 'package:restaurant_management_app/providers/food_provider.dart';
-import 'package:restaurant_management_app/screens/food_screen.dart';
+import 'package:restaurant_management_app/providers/order_provider.dart';
+import 'package:restaurant_management_app/screens/auth_check_screen.dart';
+//import 'package:restaurant_management_app/screens/food_screen.dart';
+//import 'package:restaurant_management_app/screens/login_screen.dart';
 
 void main() {
   runApp(
@@ -10,6 +14,8 @@ void main() {
       providers: [
         ChangeNotifierProvider(create: (_) => FoodProvider()),
         ChangeNotifierProvider(create: (_) => CategoryProvider()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => OrderProvider()),
       ],
       child: const MyApp(),
     ),
@@ -23,7 +29,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Restaurant Management',
-      home: const FoodScreen(),
+      home: const AuthCheckScreen(),
     );
   }
 }
