000695a15868c0320dfd238f1fb6f9d8bda7b392
RawalAnuma <rawalanuma@gmail.com>
2026-08-12 10:08:18 +0545

    feat: add authentication provider

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/user_model.dart';
+import '../services/api_service.dart';
+
+class AuthProvider extends ChangeNotifier {
+  final ApiService apiService = ApiService();
+
+  UserModel? _user;
+  bool _isLoading = false;
+  String? _token;
+
+  UserModel? get user => _user;
+  bool get isLoading => _isLoading;
+
+  Future<void> login({required String email, required String password}) async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      final response = await apiService.post('/login', {
+        'email': email,
+        'password': password,
+      });
+
+      _token = response['token'];
+
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', _token!);
+
+      //debugPrint('Token: $_token');
+
+      if (response['user'] != null) {
+        _user = UserModel.fromJson(response['user']);
+      }
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> register({
+    required String name,
+    required String email,
+    required String password,
+    required String passwordConfirmation,
+  }) async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      final response = await apiService.post('/register', {
+        'name': name,
+        'email': email,
+        'password': password,
+        'password_confirmation': passwordConfirmation,
+      });
+
+      _token = response['token'];
+
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', _token!);
+
+      if (response['user'] != null) {
+        _user = UserModel.fromJson(response['user']);
+      }
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> logout() async {
+    try {
+      await apiService.post('/logout', {});
+    } finally {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.remove('token');
+
+      _token = null;
+      _user = null;
+
+      notifyListeners();
+    }
+  }
+
+  Future<void> getCurrentUser() async {
+    try {
+      final response = await apiService.get('/user');
+
+      _user = UserModel.fromJson(response);
+
+      notifyListeners();
+    } catch (e) {
+      debugPrint('Error fetching current user: $e');
+    }
+  }
+
+  Future<bool> isLoggedIn() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getString('token') != null;
+  }
+}
