8f6b5f5643a79a2bf55b3d03baa1e9c139b978b3
RawalAnuma <rawalanuma@gmail.com>
2026-08-12 10:08:24 +0545

    feat: add order provider

---
+++ lib/providers/order_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+
+import '../models/order_model.dart';
+import '../services/api_service.dart';
+
+class OrderProvider extends ChangeNotifier {
+  final ApiService apiService = ApiService();
+
+  List<OrderModel> _orders = [];
+  bool _isLoading = false;
+
+  List<OrderModel> get orders => _orders;
+  bool get isLoading => _isLoading;
+
+  Future<void> fetchOrders() async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      final response = await apiService.get('/orders');
+
+      final List data = response['data'];
+
+      _orders = data.map((order) => OrderModel.fromJson(order)).toList();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
